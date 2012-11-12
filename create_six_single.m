e241368747595e173418d6b6514370fb9cc9ba33
John Walker <jw4610@ic.ac.uk>
2012-11-12 12:06:22 +0000

    skeleton structure

---
+++ create_six_single.m
@@ -0,0 +1,11 @@
+function [ nets ] = create_six_single( x, y )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+% Will be turned into a length 6 vector contraining 6 networks 
+nets = []; 
+
+
+end
+
