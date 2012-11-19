af5aa46e333a99bc8d1caf9c28b64ffbb31b1940
Giovanni Charles <giovanni.charles@gmail.com>
2012-11-19 18:36:09 +0000

    optimisation

--- fitness.m
+++ fitness.m
@@ -7,8 +7,9 @@ function [ f ] = fitness(x2, y2, params)
     testing_examples = x2(:,(boundary + 1):examples);
     testing_targets = y2(:,(boundary + 1):examples);
     
-    n = build_network(train_examples,train_targets, params);
+    n = build_network_gio(train_examples,train_targets, params);
     predictions = sim(n, testing_examples);
     f = f1(NNout2labels(predictions), NNout2labels(testing_targets));
     f = 100 - mean(f);
+    disp(f);
 end
